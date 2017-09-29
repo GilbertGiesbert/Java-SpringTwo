83fd0bd4b342b2219faa418ac8cee08474875dd5
Bli <bla@blubb.com>
2017-09-29 16:22:12 +0200

    extracted database.properties

--- core/src/main/java/joern/java/spring/two/core/config/PersistenceConfig.java
+++ core/src/main/java/joern/java/spring/two/core/config/PersistenceConfig.java
@@ -3,10 +3,15 @@ package joern.java.spring.two.core.config;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.commons.lang3.StringUtils;
 import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -15,17 +20,34 @@ import joern.java.spring.two.core.dao.UserDAOImpl;
 
 @Configuration
 @EnableTransactionManagement
+@PropertySource("classpath:database/database.properties")
 public class PersistenceConfig {
+	
+    private static final Logger L = LoggerFactory.getLogger(PersistenceConfig.class);
+    
+    @Autowired
+    private Environment env;
+
 	
 	@Bean(name = "dataSource")
 	public DataSource getDataSource() {
 		
+        String driverClassName = StringUtils.trimToNull(env.getProperty("database.driverClassName"));
+        String url = StringUtils.trimToNull(env.getProperty("database.url"));
+        String username = StringUtils.trimToNull(env.getProperty("database.username"));
+        String password = StringUtils.trimToNull(env.getProperty("database.password"));
+        
+        L.debug("driverClassName: "+driverClassName);
+        L.debug("url: "+url);
+        L.debug("username: "+username);
+        L.debug("password not blank: "+StringUtils.isNotBlank(password));
+		
 	    BasicDataSource dataSource = new BasicDataSource();
-	    dataSource.setDriverClassName("org.postgresql.Driver");
-	    // jdbc:postgresql://host:port/database
-	    dataSource.setUrl("jdbc:postgresql://localhost:5432/spring2");
-	    dataSource.setUsername("postgres");
-	    dataSource.setPassword("geheim123");
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
 	    return dataSource;
 	}
 	
