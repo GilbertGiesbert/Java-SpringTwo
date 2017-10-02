ba97f6234ab373ff6615edada0cb74dc33be0609
Bli <bla@blubb.com>
2017-10-02 09:29:03 +0200

    replaced deprecated hibernate criteria

--- core/src/main/java/joern/java/spring/two/core/dao/UserDAOImpl.java
+++ core/src/main/java/joern/java/spring/two/core/dao/UserDAOImpl.java
@@ -2,9 +2,16 @@ package joern.java.spring.two.core.dao;
 
 import java.util.List;
 
-import org.hibernate.Criteria;
-import org.hibernate.Query;
+import javax.persistence.NoResultException;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,11 +21,12 @@ import joern.java.spring.two.core.model.User;
 @Repository
 public class UserDAOImpl implements UserDAO {
 	
+	private static final Logger L = LoggerFactory.getLogger(UserDAOImpl.class);
+	
     @Autowired
     private SessionFactory sessionFactory;
  
     public UserDAOImpl() {
-         
     }
      
     public UserDAOImpl(SessionFactory sessionFactory) {
@@ -27,13 +35,18 @@ public class UserDAOImpl implements UserDAO {
  
     @Override
     @Transactional
-    public List<User> list() {
-        @SuppressWarnings("unchecked")
-        List<User> listUser = (List<User>) sessionFactory.getCurrentSession()
-                .createCriteria(User.class)
-                .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY).list();
- 
-        return listUser;
+    public List<User> list() {      
+
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<User> query = builder.createQuery(User.class);
+        Root<User> root = query.from(User.class);
+        query.select(root);
+        
+        Query<User> q = session.createQuery(query);
+        List<User> list = q.getResultList();
+        
+        return list;
     }
  
     @Override
@@ -53,16 +66,21 @@ public class UserDAOImpl implements UserDAO {
     @Override
     @Transactional
     public User get(int id) {
-        String hql = "from User where id=" + id;
-        Query query = sessionFactory.getCurrentSession().createQuery(hql);
-         
-        @SuppressWarnings("unchecked")
-        List<User> listUser = (List<User>) query.list();
-         
-        if (listUser != null && !listUser.isEmpty()) {
-            return listUser.get(0);
+    	
+    	Session session = sessionFactory.getCurrentSession();
+    	CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<User> query = builder.createQuery(User.class);
+        Root<User> root = query.from(User.class);
+        query.select(root).where(builder.equal(root.get("id"), id));
+        
+        Query<User> q = session.createQuery(query);
+        User user = null;
+        
+        try {
+        	user = q.getSingleResult();
+        }catch(NoResultException e) {
+        	L.debug("Failed to get user with id="+id+" from database.", e);
         }
-         
-        return null;
+        return user;
     }
 }
